5d56b726da42d2217d2285a17bd09f8f2a2f1032
Danny Cairns <dpcairns@gmail.com>
2022-05-10 12:01:49 -0700

    fix lint

--- functions/top-books-endpoint.js
+++ functions/top-books-endpoint.js
@@ -3,23 +3,27 @@ const cheerio = require('cheerio');
 
 require('dotenv').config();
 
+function mungeHTMLText(text) {
+  const $ = cheerio.load(text);
+
+  const data = $('ol > li')
+    .toArray()
+    .map(li => $(li).find('a'))
+    .filter(a => a.attr('href').split('/')[1] === 'ebooks')
+    .map(a => ({
+      title: a.text(),
+      id: a.attr('href').split('/')[2]
+    }));
+
+  return data;
+}
 
 exports.handler = async (event, context) => {
   try {
     const response = await fetch('https://www.gutenberg.org/browse/scores/top');
     const responseText = await response.text();
 
-    const $ = cheerio.load(responseText);
-
-    const data = $('ol > li')
-      .toArray()
-      .map(li => $(li).find('a'))
-      .filter(a => a.attr('href').split('/')[1] === 'ebooks')
-      .map(a => ({
-        title: a.text(),
-        id: a.attr('href').split('/')[2]
-      }));
-
+    const data = mungeHTMLText(responseText);
 
     const jsonIds = JSON.stringify({ data });
     
