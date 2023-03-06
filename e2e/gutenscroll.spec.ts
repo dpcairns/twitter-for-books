d0721465d1897f57ac6818c77dd54bd43b5b3eb1
Danny Cairns <dpcairns@gmail.com>
2023-03-05 18:48:53 -0800

    add screenshots

--- e2e/gutenscroll.spec.ts
+++ e2e/gutenscroll.spec.ts
@@ -1,20 +1,21 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
-  await page.goto('/');
-  await page.getByRole('link', { name: 'A Room with a View by E. M. Forster' }).click();
-  await page.getByText('show more tweets').click();
-  await page.getByText('show more tweets').click();
-  await page.getByText('show more tweets').click();
-  await page.getByText('X', { exact: true }).click();
-});
 
-test('test2', async ({ page}) => {
+test('front page lets us filter down books', async ({ page}) => {
   await page.goto('/');
   await page.getByLabel('Filter Books').click();
   await page.getByLabel('Filter Books').fill('mob');
+
+  await page.waitForLoadState('networkidle');
+  await expect(page).toHaveScreenshot({ maxDiffPixels: 100 });
+
   await page.getByRole('link', { name: 'Moby Dick; Or, The Whale by Herman Melville' }).click();
   const allTweets = page.locator('.tweet')
+
+  await allTweets.nth(1).waitFor();
+  await page.waitForLoadState('networkidle');
+
+  await expect(page).toHaveScreenshot({ maxDiffPixels: 6000 });
 
   await expect(allTweets.nth(2)).toContainText('States, you will have to check the laws of the country where you are located before using this eBook. Title: Moby-Dick; or The Whale Author: Herman Melville Release Date: June, 2001 [eBook #2701] [Most 3/5867');
   await page.getByText('show more tweets').click();
@@ -23,20 +24,21 @@ test('test2', async ({ page}) => {
   await page.getByText('show more tweets').click();
 
   await expect(allTweets).toHaveCount(7);
-})
 
-
-test('the front page should wait for loading and match the screenshot', async ({ page }) => {
-  await page.goto('/');
-  await page.getByText('A Room with a View by E.M. Forester').waitFor();
-  await expect(page).toHaveScreenshot({ maxDiffPixels: 1000 });
+  await page.waitForLoadState('networkidle');
+  await expect(page).toHaveScreenshot({ maxDiffPixels: 10000 });
 });
 
-test('the detail page for moby dick should match the screenshot', async ({ page }) => {
-  await page.goto('/');
-  await page.getByLabel('Filter Books').click();
-  await page.getByLabel('Filter Books').fill('mob');
-  await page.getByRole('link', { name: 'Moby Dick; Or, The Whale by Herman Melville' }).click();
-  await expect(page).toHaveScreenshot({ maxDiffPixels: 1000 });
-});
 
+test('detail page loads tweets two at a time on click', async ({ page }) => {
+  await page.goto('/');
+  await page.getByRole('link', { name: 'A Room with a View by E. M. Forster' }).click();
+  await page.getByText('show more tweets').click();
+  await page.getByText('show more tweets').click();
+  await page.getByText('show more tweets').click();
+  await page.getByText('X', { exact: true }).click();
+  await page.waitForLoadState('networkidle');
+
+  await expect(page).toHaveScreenshot({ maxDiffPixels: 9000 });
+
+});
