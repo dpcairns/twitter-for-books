65b7bb8bb938f3f95328a1a75cf7cd40117091a5
Danny Cairns <dpcairns@gmail.com>
2023-03-07 12:58:33 -0800

    add capactior

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.gutenscroll.app',
+  appName: 'Gutenscroll - Doomscroll the classics',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
